e285b45fb3c2e491198f86c85194cc17d2eb5b2e
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2021-08-19 16:11:29 +0200

    Added recipe response

--- server/src/main/scala/com/lunatech/goldenalgo/onboarding/Controller.scala
+++ server/src/main/scala/com/lunatech/goldenalgo/onboarding/Controller.scala
@@ -4,12 +4,16 @@ import akka.http.scaladsl.server.Directives._
 import scala.concurrent.ExecutionContext
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
+import io.circe.syntax._
 
 class Controller()(implicit val ec: ExecutionContext) {
 
+  // TODO: replace this hard-coded value by CRUD operations
+  val recipe1 = Recipe("recipe1-id", "recipe1-name", Seq("ingredient1", "ingredient2"), Seq("instruction1", "instruction2"))
+
   val recipes = path("recipes") {
     get {
-      complete(HttpEntity(ContentTypes.`application/json`, """{"recipes":"yummy"}"""))
+      complete(HttpEntity(ContentTypes.`application/json`, Seq(recipe1).asJson.noSpaces))
     }
   }
 
